1f7ff0e825f5a4d15956c045436b47e3b0b41e16
root <rohitagrohia07@gmail.com>
2021-06-05 10:04:56 +0530

    delete useless modules

--- app/models/like.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-var LikeSchema = mongoose.Schema({
-    user_id: { type: mongoose.Schema.ObjectId, required: true},
-    post_id: { type: mongoose.Schema.ObjectId, required: true},
-    time:{type: Number, default: Date.now},        
-}, {
-    timestamps: true
-});
-module.exports = mongoose.model('like',LikeSchema);
